e8059e354bcd7c38329bb8b0a96c50c815eb9209
Enes AGIR <enes.agir.7@gmail.com>
2022-08-12 23:07:05 +0200

    Create Sidebar.tsx

---
+++ component/Sidebar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Sidebar = () => {
+  return (
+    <div>
+      <img src='https://github.com/EnesAgir-7/React_Portfolio/blob/main/img/me.jpg?raw=true' alt="user avatar" />
+      <h3>
+        <span>Enes</span>
+        AĞIR
+      </h3>
+      <p>Full-Stack Developer</p>
+      <p>Download Resume</p>
+    </div>
+  )
+}
