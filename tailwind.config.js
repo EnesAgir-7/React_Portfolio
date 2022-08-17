65590beec5834cb95646169deaf66f447446507b
Enes AGIR <enes.agir.7@gmail.com>
2022-08-17 15:03:11 +0200

    delete

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
