a7f4f8003c0c0c21ce1353fb46e042e161f9a0f7
Enes AGIR <enes.agir.7@gmail.com>
2022-08-18 13:54:18 +0200

    pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { Route, Routes } from "react-router-dom";
 import './App.css';
 import Nav from './common/Nav';
+import Home from './Pages/Home/Home';
+
 
 function App() {
   return (
     <div>
       <Nav/>
+      <Home/>
+      {/* <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes> */}
     </div>
   );
 }
